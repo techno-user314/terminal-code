e9fcd6513b7de9dd00d24747956e7388b398d24e
techno-user314 <zharwood314@gmail.com>
2025-09-28 17:51:49 -0600

    Add advancements and progression tree

--- src/scripts/advance.js
+++ src/scripts/advance.js
@@ -1,13 +1,46 @@
 import { commands, setTip } from "./main.js";
 
+let advancementNum = 0;
+
 const advance = {
-  observeCommand(name, args) {
-    if (name === "help") {
-      commands["echo"].enabled = true;
-      setTip("Say 'hi'")
+  observeCommand(cmd, args) {
+    console.log("User ordered: ", advancementNum, cmd, args);
+    
+    // Check for filled advancement requirements:
+    if (commands["echo"].use_count > 0) {
+        alert("Achievment Unlocked: You're learning...");
+    }
+    if (commands["clear"].use_count > 10) {
+        alert("Achievment Unlocked: Janitor\n\nYou used the 'clear' command over 10 times");
+    }
+    
+    // Main game progression tree
+    advancementNum += 1;
+    switch (advancementNum) {
+      case 1:
+        if (cmd === "help") {
+          commands["echo"].enabled = true;
+          setTip("Now, say 'Hello World!'");
+        }
+        break;
+        
+      case 2:
+        if (cmd === "echo") {
+          setTip("Cleanup time!");
+        }
+        break;
+        
+      case 3:
+        if (cmd === "clear") {
+          setTip("");
+        }
+        break;
+      
+      default:
+        advancementNum -= 1;
+        break;
     }
   }
 };
 
 export { advance };
-
