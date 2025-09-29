111f92ab0c41de298f2a84b5bac3f26a34c7c3b5
techno-user314 <zharwood314@gmail.com>
2025-09-28 20:02:05 -0600

    Fix achievments bug
    Any gained achievements would pop up every command thereafter - very annoying ;-)

--- src/scripts/advance.js
+++ src/scripts/advance.js
@@ -1,43 +1,50 @@
 import { commands, setTip } from "./main.js";
 
 let advancementNum = 0;
+const granted = [false, false]
 
 const advance = {
   observeCommand(cmd, args) {
     console.log("User ordered: ", advancementNum, cmd, args);
     
     // Check for filled advancement requirements:
-    if (commands["echo"].use_count > 0) {
-        alert("Achievment Unlocked: You're learning...");
+    if (cmd in commands) {
+      commands[cmd].use_count += 1;
     }
-    if (commands["clear"].use_count > 10) {
-        alert("Achievment Unlocked: Janitor\n\nYou used the 'clear' command over 10 times");
+    if (commands["echo"].use_count === 1 && !granted[0]) {
+      granted[0] = true;
+      alert("Achievment Unlocked: You're learning...");
+    }
+    if (commands["clear"].use_count === 10 && !granted[1]) {
+      granted[1] = true;
+      alert("Achievment Unlocked: Janitor\n\nYou used the 'clear' command 10 times");
     }
     
     // Main game progression tree
-    advancementNum += 1;
     switch (advancementNum) {
-      case 1:
+      case 0:
         if (cmd === "help") {
           commands["echo"].enabled = true;
           setTip("Now, say 'Hello World!'");
+          advancementNum += 1;
+        }
+        break;
+        
+      case 1:
+        if (cmd === "echo" && JSON.stringify(args) === JSON.stringify(["Hello", "World!"])) {
+          setTip("Cleanup time!");
+          advancementNum += 1;
         }
         break;
         
       case 2:
-        if (cmd === "echo") {
-          setTip("Cleanup time!");
+        if (cmd === "clear") {
+          setTip("");
+          advancementNum += 1;
         }
         break;
         
-      case 3:
-        if (cmd === "clear") {
-          setTip("");
-        }
-        break;
-      
       default:
-        advancementNum -= 1;
         break;
     }
   }
